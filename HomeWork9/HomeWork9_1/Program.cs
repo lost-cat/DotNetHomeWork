1a00d235cffd3fe7dc1660d125062874cdab7bc0
white <2797006545@qq.com>
2021-04-22 18:04:46 +0800

    第九次作业

---
+++ HomeWork9/HomeWork9_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace HomeWork9_1
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var crawler = new Crawler("www.bilibili.com", 100);
+
+            var listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 10000);
+            listener.Start();
+
+            using (var client = listener.AcceptTcpClient())
+            {
+                Console.WriteLine($"建立连接");
+                using (var stream = client.GetStream())
+                {
+                    string message;
+                    do
+                    {
+                        var bytes = new byte[1024];
+                        var length = stream.Read(bytes, 0, 1024);
+                        message = Encoding.UTF8.GetString(bytes, 0, length);
+                        Console.WriteLine(message);
+                    } while (!string.Equals(message, "q", StringComparison.Ordinal));
+                }
+            }
+
+            listener.Stop();
+        }
+    }
+}
---
+++ HomeWork9/HomeWork9_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace HomeWork9_2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (TcpClient client = new TcpClient("127.0.0.1", 10000))
+            {
+                Console.WriteLine($"连接成功输入信息:");
+                using (var stream = client.GetStream())
+                {
+                    string message;
+                    do
+                    {
+                        message = Console.ReadLine();
+                        var bytes = Encoding.UTF8.GetBytes(message ?? string.Empty);
+                        stream.Write(bytes, 0, bytes.Length);
+                    } while (message != "q");
+                }
+            }
+        }
+    }
+}
